50023993918ae2f058d52a07aecd34962c6b65c8
gordon0724 <54224954+gordon0724@users.noreply.github.com>
2019-08-18 11:54:18 +0800

    Update main.py

--- main.py
+++ main.py
@@ -37,4 +37,4 @@ def info():
   fill = []
   return render_template("info.html", cat=fill)
 if __name__ == "__main__":
-  app.run(debug=True, host="0.0.0.0", port="3000")
+    app.run(debug=True, host="0.0.0.0", port="3000")
